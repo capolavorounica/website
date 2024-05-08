ddffce95a2c99d811fd024f3dae0b5620f12b313
runzac <91915526+alerunza@users.noreply.github.com>
2024-05-08 17:29:32 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <script setup>
-import Hero from './components/Hero.vue';
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <div class="bg-[#faffff]">
     <Navbar />
-    <Hero />
-    <!-- <HelloWorld msg="Vite + Vue" /> -->
+    <RouterView />
     <Footer />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

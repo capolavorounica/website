80df1e4f3b288367fd28ecc1fc2ce3dbe1176191
runzac <91915526+alerunza@users.noreply.github.com>
2024-05-10 10:32:10 +0200

    todo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-    <footer className="footer footer-center p-4 bg-base-100 text-base-content">
+    <footer className="footer footer-center p-4 bg-[#f5eedb1e] text-base-content">
         <aside>
             <p>Copyright © 2023 - <span class="font-bold">M. Turco</span> & <span class="font-bold">A. Runza</span> from
                 4°B.</p>
